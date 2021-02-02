8dd81a374f1819cce64e115ca31877deec6af5f6
Randomcloc <abhijeet.suryawanshi@ucdconnect.ie>
2021-02-02 16:29:36 +0000

    CMake Files Added

---
+++ cmake-build-debug/CMakeFiles/Scrabble.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/abhijeet/ucd/projects/Scrabble/game.c" "/Users/abhijeet/ucd/projects/Scrabble/cmake-build-debug/CMakeFiles/Scrabble.dir/game.c.o"
+  "/Users/abhijeet/ucd/projects/Scrabble/generateLetters.c" "/Users/abhijeet/ucd/projects/Scrabble/cmake-build-debug/CMakeFiles/Scrabble.dir/generateLetters.c.o"
+  "/Users/abhijeet/ucd/projects/Scrabble/main.c" "/Users/abhijeet/ucd/projects/Scrabble/cmake-build-debug/CMakeFiles/Scrabble.dir/main.c.o"
+  "/Users/abhijeet/ucd/projects/Scrabble/readScores.c" "/Users/abhijeet/ucd/projects/Scrabble/cmake-build-debug/CMakeFiles/Scrabble.dir/readScores.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Scrabble.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Scrabble.dir/game.c.o"
+  "CMakeFiles/Scrabble.dir/generateLetters.c.o"
+  "CMakeFiles/Scrabble.dir/main.c.o"
+  "CMakeFiles/Scrabble.dir/readScores.c.o"
+  "Scrabble"
+  "Scrabble.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Scrabble.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
